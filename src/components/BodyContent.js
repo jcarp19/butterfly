3994060ff392f02e7e80283bf6386891070fbb00
jcarp19 <jim@carpenteronline.us>
2022-09-14 07:24:59 -0400

    new logo and headline media queries

--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -1,12 +1,13 @@
 import './BodyContent.css';
-import ButterflyLogo from '../images/butterfly-logo.svg';
+// import ButterflyLogo from '../images/butterfly-logo.svg';
+import ButterflyLogoAlt from '../images/butterfly-logo-alt.svg';
 
 export default function BodyContent() {
     return (
         <div className="wrapper">
             <div className="main-content">
                 <div className="main-branding">
-                    <img className='main-logo' aria-label='Butterfly Mirror Logo' src={ButterflyLogo} alt="Butterfly Mirror Logo" />
+                    <img className='main-logo' aria-label='Butterfly Mirror Logo' src={ButterflyLogoAlt} alt="Butterfly Mirror Logo" />
                     <div className="main-headline"><span className='main-headline__title'>web design & development</span></div>
                 </div>
                 <div className="main-cta">
