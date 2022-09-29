9f2bbc335a5358c7d14ac49d5c055db7c1216743
jcarp19 <jim@carpenteronline.us>
2022-09-29 07:04:00 -0400

    removed Header from App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import BodyContent from './components/BodyContent';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <BodyContent />
     </div>
   );
