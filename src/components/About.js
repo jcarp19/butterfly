6b514b36fc530e7194a1d0d01e6f949564ceb7e3
jcarp19 <jim@carpenteronline.us>
2022-09-29 06:58:18 -0400

    starting on About

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,22 @@
 
 const About = () => {
     return (
-    <section className="row about">
-        <div className="col-1-1">
-            <h2>Who we are</h2>
+    <section className="row section-about">
+        <>
+        <div className="col-1-2">
+            <h2>a little about who we are</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, ullam, ab officiis incidunt, sequi nulla consectetur perferendis ut mollitia ratione distinctio. Autem eaque quia magnam ad, voluptatum pariatur inventore corporis.</p>
             <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
         </div>
-
+        <div className="col-1-2">
+            <div className="composition">
+                <img src="" alt="Web design sample" />
+                <img src="" alt="Web design sample" />
+                <img src="" alt="Web design sample" />
+            </div>
+        </div>
+        </>
+        
     </section>
     )
 }
--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -8,17 +8,15 @@ export default function BodyContent() {
         <>
         
         <section className="row">
-            {/* <div className="bg-image" style={{ backgroundImage: `url(${background})` }}>
-            </div> */}
             <div className="col-1-1 main-content">
-                <div className="main-branding">
-                    <img className='butterfly-icon' src={ButterflyIcon} alt="Butterfly Mirrors Logo" />
-                    <img className='main-logo' aria-label='Butterfly Mirror Logo' src={ButterflyLogoAlt} alt="Butterfly Mirror Logo" />
-                    <div className="main-headline"><span className='main-headline__title'>web design & development</span></div>
+                <div className="row phone">
+                    <span className="material-icons phone-icon">call</span>
+                    <span className='phone-number'>(248) 953-4315</span>
                 </div>
-                <div className="main-cta">
-                    <a className="btn" href='https://butterflymirrors.com' aria-label='Learn More' title="Learn More button">Learn More</a>
-                </div>
+                <img className='butterfly-icon' src={ButterflyIcon} alt="Butterfly Mirrors Logo" />
+                <img className='main-logo' aria-label='Butterfly Mirror Logo' src={ButterflyLogoAlt} alt="Butterfly Mirror Logo" />
+                <h1 className='main-headline__title'>web design & development</h1>
+                <a className="btn" href='https://butterflymirrors.com' aria-label='Learn More' title="Learn More button">Learn More</a>
             </div>
         </section>
     
